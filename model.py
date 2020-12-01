547a907de1471387732ebd2b842ecf14877b4f2e
yyy1990 <yyy_cn@qq.com>
2020-12-01 17:42:57 +0800

    Update model.py

--- model.py
+++ model.py
@@ -263,11 +263,15 @@ def sent_classi(sentence, vector, model, device = None, cut = True):
 def get_sentiment(sentence, vector_path = './data/sgns.sogou.bigram', model_paths = ['./data/set0_f3.pth', './data/set1_f5.pth', './data/set2_f7.pth']):
     '''
     综合多个模型进行情感分析,将sigmoid之后分数取平均再取整
+    vector_path: 预训练词向量路径或预训练词向量
     sentence: 待分析的句子
     model_paths: 模型存储路径列表
     '''
     device = set_device()
-    vector = KeyedVectors.load_word2vec_format(vector_path,binary=False, encoding="utf8",  unicode_errors='ignore')
+    if type(vector_path) == str:
+        vector = KeyedVectors.load_word2vec_format(vector_path,binary=False, encoding="utf8",  unicode_errors='ignore')
+    else:
+        vector = vector_path
     score_sum = 0
     for path in model_paths:
         rnn = torch.load(path)
